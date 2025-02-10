6f26b61e70acb442866f7baaa2a2a21e46db0e5c
Sandhiya-sysarc <sandhiyajayanthi03@gmail.com>
2025-02-10 16:15:48 +0530

    feat : otp component implementation

---
+++ projects/test-otp/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of test-otp
+ */
+export * from './lib/custom-btn/custom-btn.component';
+export * from './lib/test-otp.service';
+export * from './lib/test-otp.component';
+export * from './lib/otp/otp.component';
+export * from './lib/test-otp.module';
+// comitting on otp-component branch
